f1a8660c51247782c7f41543b775fb31dd993a2f
Donovan So <donovanso@yahoo.com.hk>
2023-02-28 15:45:39 -0800

    BRI-344 add simple code preview to core plugin

--- core/code.ts
+++ core/code.ts
@@ -3,10 +3,23 @@ import { generateStyledBricksNode } from "./src/StyledBricksNode";
 
 figma.showUI(__html__);
 
+// restore previous window size
+figma.clientStorage.getAsync("size").then((size) => {
+  if (size) figma.ui.resize(size.w, size.h);
+});
+
 figma.ui.onmessage = async (msg) => {
   if (msg.type === "generate-bricks-nodes") {
     const result = await generateBricksTree(figma.currentPage.selection);
-    const styledNodes = await Promise.all(result.map(generateStyledBricksNode));
-    console.log(styledNodes);
+    const nodes = await Promise.all(result.map(generateStyledBricksNode));
+    console.log(nodes);
+    figma.ui.postMessage({
+      type: "render-nodes",
+      nodes,
+    });
+  }
+  if (msg.type === "resize") {
+    figma.ui.resize(msg.size.w, msg.size.h);
+    figma.clientStorage.setAsync("size", msg.size).catch((err) => {}); // save size
   }
 };
